9b544bb0ff1bc06b08519cc4860e943002fa77be
lindesaysh <lass@st-and.ac.uk>
2015-05-12 10:33:17 +0100

    part commit may 15

--- salsasplitcode/get.measure_2d.R
+++ salsasplitcode/get.measure_2d.R
@@ -1,13 +1,4 @@
-#' Function to get the statistic required to determine fit of the models
-#' 
-#' @author Cameron Walker, Department of Enginering Science, University of Auckland.
-#' 
-#' @export
-#' 
-
-###################################################################################
-
-"get.measure_2d" <- function(fitnessMeasure,measures,out.lm, data, dists,aR,radii,radiusIndices){
+get.measure_2d<- function(fitnessMeasure,measures,out.lm, data, dists,aR,radii,radiusIndices){
   #print("ooooooooooooooooooooooooooooooooooooooo")
   #print("Getting measure...")
   #print("ooooooooooooooooooooooooooooooooooooooo")
@@ -26,21 +17,21 @@
   
   if(fitnessMeasure=="QAIC"){       
     
-    if(baseModel$family[1]=="quasipoisson"){
+    if(out.lm$family[1]=="quasipoisson"){
       PoisMod<-update(out.lm, round(.)~., family=poisson)
       fitStat <- QAIC(PoisMod, chat = summary(out.lm)$dispersion)}
     
-    if(baseModel$family[1]=="quasibinomial"){
+    if(out.lm$family[1]=="quasibinomial"){
       BinMod<-update(out.lm, family=binomial)
       fitStat <- QAIC(BinMod, chat = summary(out.lm)$dispersion)}
   }
   
   if(fitnessMeasure=="QAICc"){       
-    if(baseModel$family[1]=="quasipoisson"){
+    if(out.lm$family[1]=="quasipoisson"){
       PoisMod<-update(out.lm, family=poisson)
       fitStat <- QAICc(PoisMod, chat = summary(out.lm)$dispersion)}
     
-    if(baseModel$family[1]=="quasibinomial"){
+    if(out.lm$family[1]=="quasibinomial"){
       BinMod<-update(out.lm, family=binomial)
       fitStat <- QAICc(BinMod, chat = summary(out.lm)$dispersion)}
     
@@ -62,31 +53,19 @@
     fitStat<- mean(getCV_type2(folds = 5, out.lm))
   }
   
-  
   if(fitnessMeasure=="CV"){  
-    
-    nfolds<-length(unique(data$foldid))
-    store<- matrix(0, nrow=nfolds, ncol=1)
-    d2k<-dists
-    for(f in 1:nfolds){
-      dists<-d2k[data$foldid!=f,]
-      foldedFit<- update(out.lm, .~., data=data[data$foldid!=f,])
-      if(length(coef(foldedFit))==1){
-        dists<-d2k[data$foldid==f,]
-        predscv<- exp(as.matrix(model.matrix(out.lm)[data$foldid==f])%*%coef(foldedFit)) * exp(baseModel$offset)[data$foldid==f]
-      }else{  
-        dists<-d2k[data$foldid==f,]
-        predscv<- exp(model.matrix(out.lm)[data$foldid==f,]%*%coef(foldedFit)) * exp(out.lm$offset)[data$foldid==f]
-      }        
-      store[f]<- mean((data$response[data$foldid==f]-predscv)**2)
-    }
-    
-    fitStat<- mean(store)
+    fitStat <- getCV_CReSS(data, out.lm, splineParams)
   }
+  
+  
+  if(fitnessMeasure=="PRESS"){  
+    fitStat <- getPRESS_CReSS(data, out.lm)
+  }
+  
   
   # calculate a QIC with bayesian penalty
   if(fitnessMeasure=="QICb"){       
-    fitStat <- QICb(out.lm$data$response, fitted(out.lm), length(out.lm$coeff), length(out.lm$data$response))
+    fitStat <- QICb(out.lm)
   }
   
   # cat("Evaluating new fit: ", fitStat, "\n")
--- salsasplitcode/move.knot_2d.R
+++ salsasplitcode/move.knot_2d.R
@@ -1,12 +1,3 @@
-#' Function to move knots around and re-fit model to find best solution
-#' 
-#' @author Cameron Walker, Department of Enginering Science, University of Auckland.
-#' 
-#' @export
-#' 
-
-
-
 "move.knot_2D" <- function(radii,invInd,dists,explData,index,fitnessMeasure,BIC,aR,point,
                            response,explanatory,out.lm,improve,improveEx,track, maxKnots,tol=0,baseModel,radiusIndices,models, interactionTerm, data){
   attributes(baseModel$formula)$.Environment<-environment()
@@ -25,18 +16,20 @@
     
     tempRadii = radiusIndices
     tempRadii[i] = ceiling(length(radii)/2)
+    
     output = fit.thinPlate_2d(fitnessMeasure, dists,invInd[tempR],radii,baseModel,tempRadii,models, fitStat, interactionTerm, data)
     initModel = output$currentModel
     models = output$models
     initBIC = get.measure_2d(fitnessMeasure,fitStat,initModel, data,  dists, invInd[tempR],radii, tempRadii)$fitStat
-    #         out<-choose.radii(initBIC,i,tempRadii,radii,initModel,dists,invInd[tempR],baseModel,fitnessMeasure,response)
+
     out<-choose.radii(initBIC,1:length(radiusIndices),tempRadii,radii,initModel,dists,invInd[tempR],baseModel, fitnessMeasure,response,models, interactionTerm, data)
+    
     tempRadii=out$radiusIndices
     tempOut.lm=out$out.lm
     models=out$models
     output<-get.measure_2d(fitnessMeasure,fitStat,tempOut.lm, data,  dists, invInd[tempR],radii, tempRadii)
-    #fitStat<-output$tempMeasure
     tempMeasure<-output$fitStat
+    
     if (tempMeasure+tol < fitStat) {
       out.lm <- tempOut.lm
       fitStat<-tempMeasure
@@ -56,15 +49,19 @@
     tempR<-c(aR,point[index])
     tempRadii = c(radiusIndices,(1:length(radii))[ceiling(length(radii)/2)])
     print("Adding knot...")
-    #out<-choose.radii(fitStat,length(radiusIndices)+1,tempRadii,radii,out.lm,dists,invInd[tempR],baseModel,fitnessMeasure,response)
-    out<-choose.radii(fitStat,1:length(tempRadii),tempRadii,radii,out.lm,dists,invInd[tempR],baseModel,fitnessMeasure,response,models, interactionTerm, data)
+  
+    output = fit.thinPlate_2d(fitnessMeasure, dists,invInd[tempR],radii,baseModel,tempRadii,models, fitStat, interactionTerm, data)
+    initModel = output$currentModel
+    models = output$models
+    initBIC = get.measure_2d(fitnessMeasure,fitStat,initModel, data,  dists, invInd[tempR],radii, tempRadii)$fitStat
+    out<-choose.radii(initBIC,1:length(tempRadii),tempRadii,radii,initModel,dists,invInd[tempR],baseModel, fitnessMeasure,response,models, interactionTerm, data)
+    
     tempRadii=out$radiusIndices
     tempOut.lm=out$out.lm
-    models = out$models
-    #tempOut.lm<-fit.thinPlate_2d(dists,invInd[tempR],radii,baseModel,radiusIndices)
+    models=out$models
     output<-get.measure_2d(fitnessMeasure,fitStat,tempOut.lm, data,  dists, invInd[tempR],radii, tempRadii)
-    #fitStat<-output$tempMeasure
     tempMeasure<-output$fitStat
+    
     if (tempMeasure +tol < fitStat) {
       out.lm <- tempOut.lm
       fitStat<-tempMeasure
