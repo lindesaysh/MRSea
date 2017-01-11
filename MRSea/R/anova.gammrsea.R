7638bb04e35a02600f0b732f1d61ab0137dfd105
lindesaysh <lass@st-and.ac.uk>
2017-01-11 12:52:45 +0000

    anova to include F-test

--- MRSea/R/anova.gammrsea.R
+++ MRSea/R/anova.gammrsea.R
@@ -1,5 +1,5 @@
-
-anova.gamMRSea<-function(object, varshortnames=NULL, panelid=NULL){
+a=
+anova.gamMRSea<-function(object, varshortnames=NULL, panelid=NULL, test='Wald'){
 
   if(!is.null(object$varshortnames)){
     varshortnames=object$varshortnames
@@ -16,26 +16,80 @@ anova.gamMRSea<-function(object, varshortnames=NULL, panelid=NULL){
   panelid <- if(is.null(panelid) & is.null(object$panels)) 1:nrow(object$data) else object$panels
   vbeta<-sandcov(object, id=panelid)
   ncoefs<-table(varseq)
-  X2Vec<-NULL
-
-  for(j in 1:nvars){
-    beta0<-rep(0, length(beta))
-    beta0[varseq==j]<-1
-    #beta0<-rev(beta0)
-    zeroidx<-beta0==1
-    X2<-t(beta[zeroidx]%*% solve(vbeta[zeroidx, zeroidx, drop=FALSE]) %*% beta[zeroidx])
-    X2Vec<-c(X2Vec, X2)
+  
+  if(test=='Wald'){
+    X2Vec<-NULL
+    
+    for(j in 1:nvars){
+      beta0<-rep(0, length(beta))
+      beta0[varseq==j]<-1
+      #beta0<-rev(beta0)
+      zeroidx<-beta0==1
+      X2<-t(beta[zeroidx]%*% solve(vbeta[zeroidx, zeroidx, drop=FALSE]) %*% beta[zeroidx])
+      X2Vec<-c(X2Vec, X2)
+    }
+    
+    hasIntercept <- (length(grep("(Intercept)", names(beta))) != 0)
+    
+    if(hasIntercept){
+      dfVec<-as.vector(table(varseq[-1]))
+    }
+    
+    resdf<-dfVec
+    resdev<-X2Vec
+    table<-data.frame(resdf, resdev, 1-pchisq(resdev, resdf))
+    
   }
-
-  hasIntercept <- (length(grep("(Intercept)", names(beta))) != 0)
-
-  if(hasIntercept){
-    dfVec<-as.vector(table(varseq[-1]))
-  }
-
-  resdf<-dfVec
-  resdev<-X2Vec
-  table<-data.frame(resdf, resdev, 1-pchisq(resdev, resdf))
+  
+  if(test=='F'){
+    which.nms <- function(name) which(varseq == which(names == 
+                                                      name))
+    df.res <- df.residual(object)
+    error.SS <- sum(residuals(object, "pearson")^2, na.rm = TRUE)
+    
+    fac <- attr(terms(object), "factors")
+    names <- labels(terms(object))
+    n.terms <- length(names)
+    y <- object$y
+    if (is.null(y)) 
+      y <- model.response(model.frame(object), "numeric")
+    wt <- object$prior.weights
+    if (is.null(wt)) 
+      wt <- rep(1, length(y))
+    p <- df <- f <- SS <- rep(0, n.terms + 1)
+    f[n.terms + 1] <- p[n.terms + 1] <- NA
+    df[n.terms + 1] <- df.res
+    SS[n.terms + 1] <- error.SS
+    dispersion <- error.SS/df.res
+  
+    for (term in 1:n.terms) {
+      rels <- names[relatives(names[term], names, fac)]
+      exclude.1 <- as.vector(unlist(sapply(c(names[term], rels), 
+                                           which.nms)))
+      mod.1 <- glm.fit(x[, -exclude.1, drop = FALSE], y, wt, 
+                       offset = object$offset, family = object$family, control = object$control)
+      dev.1 <- deviance(mod.1)
+      mod.2 <- if (length(rels) == 0) 
+        object
+      else {
+        exclude.2 <- as.vector(unlist(sapply(rels, which.nms)))
+        glm.fit(x[, -exclude.2, drop = FALSE], y, wt, offset = object$offset, 
+                family = object$family, control = object$control)
+      }
+      dev.2 <- deviance(mod.2)
+      df[term] <- df.residual(mod.1) - df.residual(mod.2)
+      if (df[term] == 0) 
+        SS[term] <- f[term] <- p[term] <- NA
+      else {
+        SS[term] <- dev.1 - dev.2
+        f[term] <- SS[term]/(dispersion * df[term])
+        p[term] <- pf(f[term], df[term], df.res, lower.tail = FALSE)
+      }
+    }
+    result <- data.frame(SS, df, f, p)
+    }
+  
+  
   tl<-attr(object$terms, 'term.labels')
 
   if (length(varshortnames) > 0) {
@@ -58,10 +112,37 @@ anova.gamMRSea<-function(object, varshortnames=NULL, panelid=NULL){
     }
   }
   
+  if(test=='Wald'){
+    dimnames(table)<-list(c(tl), c("Df", "X2", "P(>|Chi|)"))
+    title <- paste("Analysis of 'Wald statistic' Table", "\nModel: ",
+                   object$family$family, ", link: ", object$family$link,
+                   "\nResponse: ", as.character(varlist[-1])[1], "\nMarginal Testing\n", "Max Panel Size = ",max(table(panelid)),"; Number of panels = ", max(panelid),"\n", sep = "")
+    result<-structure(table, heading = title, class = c("anova", "data.frame"))
+    
+  }
   
-  dimnames(table)<-list(c(tl), c("Df", "X2", "P(>|Chi|)"))
-  title <- paste("Analysis of 'Wald statistic' Table", "\nModel: ",
-                 object$family$family, ", link: ", object$family$link,
-                 "\nResponse: ", as.character(varlist[-1])[1], "\nMarginal Testing\n", "Max Panel Size = ",max(table(panelid)),"; Number of panels = ", max(panelid),"\n", sep = "")
-  structure(table, heading = title, class = c("anova", "data.frame"))
+  if(test=='F'){
+  row.names(result) <- c(tl, "Residuals")
+  names(result) <- c("SS", "Df", "F", "Pr(>F)")
+  class(result) <- c("anova", "data.frame")
+  attr(result, "heading") <- c("Analysis of Deviance Table (Type II tests)\n", 
+                               paste("Response:", as.character(varlist[-1])[1]), paste("Error estimate based on", "Pearson residuals", "\n"))
+  }
+  
+  result
+}
+
+
+
+
+relatives<-function (term, names, factors) 
+{
+  is.relative <- function(term1, term2) {
+    all(!(factors[, term1] & (!factors[, term2])))
+  }
+  if (length(names) == 1) 
+    return(NULL)
+  which.term <- which(term == names)
+  (1:length(names))[-which.term][sapply(names[-which.term], 
+                                        function(term2) is.relative(term, term2))]
 }
