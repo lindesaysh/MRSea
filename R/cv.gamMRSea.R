928cb3d25986d5f50ea510df704517b83bad184d
lindesaysh <lass@st-and.ac.uk>
2017-04-18 12:43:14 +0100

    documentation for cv.gamMRSea

--- R/cv.gamMRSea.R
+++ R/cv.gamMRSea.R
@@ -1,7 +1,29 @@
+#' Cross-validation for gamMRSea Models
+#' 
+#' This function calculates the estimated K-fold cross-validation prediction error for gamMRSea models. It has been adapted from the \link[boot]{cv.glm} function from the \code{boot} library
+#' 
+#' @param data A matrix or data frame containing the data. The rows should be cases and the columns correspond to variables, one of which is the response.
+#' @param modelobject An object of class "gamMRSea" containing the results of a gamMRSea model fitted to data.
+#' @param cost A function of two vector arguments specifying the cost function for the cross-validation. The first argument to cost should correspond to the observed responses and the second argument should correspond to the predicted or fitted responses from the generalized linear model. cost must return a non-negative scalar value. The default is the average squared error function.
+#' @param K The number of groups into which the data should be split to estimate the cross-validation prediction error. The value of K must be such that all groups are of approximately equal size. If the supplied value of K does not satisfy this criterion then it will be set to the closest integer which does and a warning is generated specifying the value of K used. The default is to set K equal to the number of observations in data which gives the usual leave-one-out cross-validation.
+#' 
+#' @details For more information please see the \link[boot]{cv.glm} function in the boot library
+#' 
+#' @examples
+#' 
+#' # load data
+#' data(ns.data.re)
+#' ns.data.re$foldid<-getCVids(ns.data.re, folds=5)
+#'  
+#' model<-gamMRSea(birds ~ observationhour + as.factor(floodebb) + as.factor(impact),  
+#'               family='poisson', data=ns.data.re)
+#' cv.gamMRSea(data=ns.data.re, modelobject = model, K=5)$delta
+#' 
+#' @export
+#' 
+#'
 
-
-
-cv.gamMRSea<-function (data, glmfit, cost = function(y, yhat) mean((y - yhat)^2), 
+cv.gamMRSea<-function (data, modelobject, cost = function(y, yhat) mean((y - yhat)^2), 
           K = n) 
 {
   call <- match.call()
@@ -22,11 +44,11 @@ cv.gamMRSea<-function (data, glmfit, cost = function(y, yhat) mean((y - yhat)^2)
   f <- ceiling(n/K)
   s <- boot:::sample0(rep(1L:K, f), n)
   n.s <- table(s)
-  glm.y <- glmfit$y
-  cost.0 <- cost(glm.y, fitted(glmfit))
+  glm.y <- modelobject$y
+  cost.0 <- cost(glm.y, fitted(modelobject))
   ms <- max(s)
   CV <- 0
-  Call <- glmfit$call
+  Call <- modelobject$call
   for (i in seq_len(ms)) {
     
     j.out <- seq_len(n)[(s == i)]
@@ -35,11 +57,11 @@ cv.gamMRSea<-function (data, glmfit, cost = function(y, yhat) mean((y - yhat)^2)
     
     Call$data <- data[j.in, , drop = FALSE]
     
-    if(!is.null(glmfit$splineParams)){
-      splineParams<-glmfit$splineParams
+    if(!is.null(modelobject$splineParams)){
+      splineParams<-modelobject$splineParams
       if(!is.null(splineParams[[1]]$dist)){
-        splineParams[[1]]$dist<-glmfit$splineParams[[1]]$dist[j.in,]
-        g2k<-glmfit$splineParams[[1]]$dist[j.out,]
+        splineParams[[1]]$dist<-modelobject$splineParams[[1]]$dist[j.in,]
+        g2k<-modelobject$splineParams[[1]]$dist[j.out,]
         Call$splineParams<-splineParams
       }
     }else{
@@ -53,8 +75,8 @@ cv.gamMRSea<-function (data, glmfit, cost = function(y, yhat) mean((y - yhat)^2)
     cost.i <- cost(glm.y[j.out], predict(object=d.glm, newdata=data[j.out, ,drop = FALSE], g2k=g2k, type = "response"))
     CV <- CV + p.alpha * cost.i
     
-    if(!is.null(glmfit$splineParams[[1]]$dist)){
-      g2k<-glmfit$splineParams[[1]]$dist
+    if(!is.null(modelobject$splineParams[[1]]$dist)){
+      g2k<-modelobject$splineParams[[1]]$dist
     }else{
       g2k<-NULL
     }
