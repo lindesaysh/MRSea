16aa5b0ccd903f7bbef52f90ce2a3894bfcdd469
lindesaysh <lass@st-and.ac.uk>
2018-04-18 17:10:47 +0100

    add gamMRSea bread function

--- R/sandcovFunc.R
+++ R/sandcovFunc.R
@@ -26,3 +26,18 @@ clsandcov   <- function(dat,fm, cluster){
   uj  <- apply(estfun(fm),2, function(x) tapply(x, cluster, sum));
   vcovCL <- dfc*sandwich(fm, meat=crossprod(uj)/N)
   return(vcovCL)}
+
+
+bread.gamMRSea<-function (x, ...) 
+{
+  if (!is.null(x$na.action)) 
+    class(x$na.action) <- "omit"
+  sx <- summary.glm(x)
+  wres <- as.vector(residuals(x, "working")) * weights(x, 
+                                                       "working")
+  dispersion <- if (substr(x$family$family, 1, 17) %in% c("poisson", 
+                                                          "binomial", "Negative Binomial")) 
+    1
+  else sum(wres^2)/sum(weights(x, "working"))
+  return(sx$cov.unscaled * as.vector(sum(sx$df[1:2])) * dispersion)
+}
