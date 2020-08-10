68ca1e97216a61835db541bfe2d54f5c7e69c4d5
CMFell <cmf21@st-andrews.ac.uk>
2020-08-11 00:25:37 +0100

    Add 15 knots for test case 4 also debug MSE to data

--- R/return.reg.spline.fit.2d.R
+++ R/return.reg.spline.fit.2d.R
@@ -60,6 +60,7 @@
   #   sink(file='salsa2d.log')
   # }
   # 
+  print("branch test_cases")
   # split out spline parameter object into its pieces
   knotDist <- splineParams[[1]]$knotDist
   radii <- splineParams[[1]]$radii
