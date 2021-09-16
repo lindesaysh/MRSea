23101f216aa716a24a248d98921075b4040eae7b
lindesaysh <lass@st-and.ac.uk>
2021-09-16 14:11:07 +0100

    Bug fix for QAIC and QBIC measures.  - ensure rounded poisson response when not using quasipoisson. removal of all multinomial lines

--- R/get.measure.R
+++ R/get.measure.R
@@ -27,96 +27,65 @@
     fitStat <- BIC(out.lm)}
   
   if(fitnessMeasure=="QAIC"){ 
-    if (isS4(out.lm)){
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
       if(out.lm$family[1]=='quasipoisson'){
-        fitStat <- QAIC(update(out.lm, round(response) ~ ., family=poisson), chat = extra)
+        fitStat <- QAIC(update(out.lm, round(response) ~ ., family=poisson), chat = initDisp)
       }else{
-        fitStat <- QAIC(out.lm, chat = extra)
+        fitStat <- QAIC(out.lm, chat = initDisp)
       }
       if(out.lm$family[1]=='quasibinomial'){
-        fitStat <- QAIC(update(out.lm,.~. ,family=binomial), chat = extra)
+        fitStat <- QAIC(update(out.lm,.~. ,family=binomial), chat = initDisp)
       }else{
-        fitStat <- QAIC(out.lm, chat = extra)
+        fitStat <- QAIC(out.lm, chat = initDisp)
       }
-    }
   }
   
   if(fitnessMeasure=="QBIC"){ 
-    if (isS4(out.lm)){
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
-      if(out.lm$family[1]=='quasipoisson'){
-        fitStat <- QAIC(update(out.lm,  round(response) ~ ., family=poisson), chat = extra, k=log(nrow(out.lm$data)))
+    if(out.lm$family[1]=='quasipoisson'){
+        fitStat <- QAIC(update(out.lm,  round(response) ~ ., family=poisson), chat = initDisp, k=log(nrow(out.lm$data)))
       }else{
-        fitStat <- QAIC(out.lm, chat = extra, k=log(nrow(out.lm$data)))
+        fitStat <- QAIC(out.lm, chat = initDisp, k=log(nrow(out.lm$data)))
       }
       if(out.lm$family[1]=='quasibinomial'){
-        fitStat <- QAIC(update(out.lm, .~.,family=binomial), chat = extra,k=log(nrow(out.lm$data)))
+        fitStat <- QAIC(update(out.lm, .~.,family=binomial), chat = initDisp,k=log(nrow(out.lm$data)))
       }else{
-        fitStat <- QAIC(out.lm, chat = extra, k=log(nrow(out.lm$data)))
+        fitStat <- QAIC(out.lm, chat = initDisp, k=log(nrow(out.lm$data)))
       }
-    }
   }
   
   if (fitnessMeasure == "newCrit") {
-    if (isS4(out.lm)) {
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
       fitStat <- mean((residuals(out.lm)/(1-influence(out.lm)$h))**2)
-    }
   }
   
   
   if(fitnessMeasure=="QAICc"){
-    if (isS4(out.lm)) {
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
-      if(out.lm$family[1]=='quasipoisson'){
-        fitStat <- QAICc(update(out.lm,  round(response) ~ ., family=poisson), chat = extra)
+       if(out.lm$family[1]=='quasipoisson'){
+        fitStat <- QAICc(update(out.lm,  round(response) ~ ., family=poisson), chat = initDisp)
       }else{
-        fitStat <- QAICc(out.lm, chat = extra)
+        fitStat <- QAICc(out.lm, chat = initDisp)
       }
       if(out.lm$family[1]=='quasibinomial'){
-        fitStat <- QAICc(update(out.lm, .~.,family=binomial), chat = extra)
+        fitStat <- QAICc(update(out.lm, .~.,family=binomial), chat = initDisp)
       }else{
-        fitStat <- QAICc(out.lm, chat = extra)
+        fitStat <- QAICc(out.lm, chat = initDisp)
       }
-    }
   }
   
   # calculates a QIC but using a bic penalty
   if(fitnessMeasure=="QICb"){  
-    if (isS4(out.lm)) {
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
       fitStat <- QICb(out.lm)
     }
-  }
   
   # Hardin and Hilbe AIC statistic.  See Hilbe 2014 modelling count data book
   if(fitnessMeasure=="AICh"){
-    if (isS4(out.lm)){
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
       fitStat<-AICh(out.lm)
     }
-  }
   
   # calculates CV using lsh code below - includes offset
   if(fitnessMeasure=="CV.offset"){
-    if (isS4(out.lm)){
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
       fitStat<- mean(getCV_type2(folds=5, out.lm))
-    }
   }
   # calculates CV using cv.glm function - no offset included
   if(fitnessMeasure=="cv.glm"){
-    if (isS4(out.lm)){
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
       if(dim(model.matrix(out.lm))[2]==1){
         data2<- data.frame(response=out.lm$y)
         textForEval<- "tempCVFit<-glm(response~1, data=data2, family=family(out.lm))" 
@@ -129,14 +98,10 @@
       eval(parse(text=textForEval))  
       require(boot)
       fitStat<-cv.glm(data2,tempCVFit, K=5)$delta[2]
-    }
   }
   
   # calculates cv with blocking structure
   if(fitnessMeasure=="CV"){
-    if (isS4(out.lm)){
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
       store<- matrix(0, nrow=nfolds, ncol=1)
       for(f in 1:nfolds){
         #explanatory<- out.lm$data
@@ -147,14 +112,9 @@
       }
       fitStat<- mean(store)
     }
-  }
   
   if(fitnessMeasure=="CV_CReSS"){
-    if (isS4(out.lm)){
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
       fitStat<-getCV_CReSS(data, out.lm)
-    }
   }
   
   if(fitnessMeasure=="cv.gamMRSea"){
@@ -173,12 +133,8 @@
     #   set.seed(cv.opts$cv.gamMRSea.seed)
     #   fitStat<-cv.glm(data2,tempCVFit, K=cv.opts$K, cost=cv.opts$cost)$delta[2] 
     # }else{
-    if (isS4(out.lm)){
-      stop('Fitness measure not supported for multinomial.  Please use AIC, AICc or BIC')
-    } else {
       set.seed(cv.opts$cv.gamMRSea.seed)
       fitStat<-cv.gamMRSea(data,out.lm, K=cv.opts$K, cost=cv.opts$cost)$delta[2]
-    }
     #}
   }
   
