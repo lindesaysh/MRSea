5ee6c8faff28ce18f6fbc2b9db12f7a1176e5977
Lindesay Scott-Hayward <lass@st-and.ac.uk>
2026-04-23 18:29:27 +0100

    Use language calls; pointProcess AIC
    To ensure model weights are retained throughout - Refactor runSALSA1D to stop building model calls with eval(parse(text=...)) and string concatenation. 1D term strings are converted to language objects via a new as_term_call helper and appended to the initial model call to create a gamMRSea baseModel directly. Model updates now use call/as.formula forms instead of parsed text. Cross-validation scoring for modelType == "pointProcess" now uses AIC with k = log(N) for model selection when removal=TRUE

--- R/runSALSA1D.R
+++ R/runSALSA1D.R
@@ -220,7 +220,7 @@ runSALSA1D<-function(initialModel, salsa1dlist, varlist, factorlist=NULL, predic
     salsa1dlist$modelType<-'regression'
   }
   initialModel$modelType = salsa1dlist$modelType
-  splineParams[[1]]$modelType= salsa1dlist$modelType
+  splineParams[[1]]$modelType = salsa1dlist$modelType
   
   terms1D <- list(length(varlist))
   
@@ -246,25 +246,60 @@ runSALSA1D<-function(initialModel, salsa1dlist, varlist, factorlist=NULL, predic
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # initial model is all variables in varlist with knots at locations in splineParams and NO 2D smooth
   require(mgcv)
-  if(fam=='BinProp'){
-    baseModel <- eval(parse(text=paste("gamMRSea(cbind(successes,failures) ~ ", paste(formula(initialModel)[3],sep=""), "+", paste(terms1D, collapse="+"),", family =", family,"(link=", link,"), data = data)", sep='')))
-  }else{
-    if(family=="Tweedie"){
-      baseModel <- eval(parse(text = paste("gamMRSea(response ~ ", 
-                                           paste(formula(initialModel)[3], sep = ""), "+", 
-                                           paste(terms1D, collapse = "+"), ", family =", 
-                                           paste0(initialModel$call[grep("tweedie", initialModel$call)]),
-                                           ", data = data)", sep = "")))  
-    }else{
-      baseModel <- eval(parse(text = paste("gamMRSea(response ~ ", 
-                                           paste(formula(initialModel)[3], sep = ""), "+", 
-                                           paste(terms1D, collapse = "+"), 
-                                           ", family =", family,
-                                           "(link=", link,
-                                           "), data = data)", sep = "")))
-    }
-    
+  # if(fam=='BinProp'){
+  #   baseModel <- eval(parse(text=paste("gamMRSea(cbind(successes,failures) ~ ", 
+  #                                      paste(formula(initialModel)[3],sep=""), "+", 
+  #                                      paste(terms1D, collapse="+"),
+  #                                      ", family =", family,
+  #                                      "(link=", link,"), data = data)", sep='')))
+  # }else{
+  #   if(family=="Tweedie"){
+  #     baseModel <- eval(parse(text = paste("gamMRSea(response ~ ", 
+  #                                          paste(formula(initialModel)[3], sep = ""), "+", 
+  #                                          paste(terms1D, collapse = "+"), 
+  #                                          ", family =", 
+  #                                          paste0(initialModel$call[grep("tweedie", initialModel$call)]),
+  #                                          ", data = data)", sep = "")))  
+  #   }else{
+  #     baseModel <- eval(parse(text = paste("gamMRSea(response ~ ", 
+  #                                          paste(formula(initialModel)[3], sep = ""), "+", 
+  #                                          paste(terms1D, collapse = "+"), 
+  #                                          ", family =", family,
+  #                                          "(link=", link,
+  #                                          "), data = data)", sep = "")))
+  #   }
+  #   
+  # }
+  
+  # change 1D terms to be a language based list so that it can be added to the formula of the initial model
+  as_term_call <- function(x) {
+    if (is.language(x)) return(x)
+    if (is.character(x)) return(str2lang(x))
+    stop("terms1D must contain character strings or language objects")
   }
+  terms1D <- lapply(terms1D, as_term_call)
+  
+  # add the 1D terms to the formula of the initial model
+  cl <- initialModel$call
+  old_rhs <- formula(initialModel)[[3]]
+  
+  new_rhs <- Reduce(
+    function(x, y) call("+", x, y),
+    terms1D,
+    init = old_rhs
+  )
+  
+  cl$formula <- as.formula(
+    call("~", formula(initialModel)[[2]], new_rhs)
+  )
+  
+  # change to gamMRSea model
+  cl[[1]] <- quote(gamMRSea)
+  
+  # create baseModel
+  baseModel <- eval(cl, parent.frame())
+  
+  
   
   if(!is.null(initialModel$cvfolds)){
     baseModel$cvfolds <- initialModel$cvfolds
@@ -276,6 +311,9 @@ runSALSA1D<-function(initialModel, salsa1dlist, varlist, factorlist=NULL, predic
     #set.seed(seed.in)
     basecoef<-length(coef(baseModel))
     cv_initial <- cv.gamMRSea(data, baseModel, K=salsa1dlist$fit.opts$K, cost=salsa1dlist$fit.opts$cost, s.eed = seed.in)$delta[2]
+    if(salsa1dlist$modelType == "pointProcess"){
+      cv_initial<- AIC(baseModel, k = log(salsa1dlist$fit.opts$N))
+    }
   }else{
     cv_initial=NULL
   }
@@ -349,12 +387,19 @@ runSALSA1D<-function(initialModel, salsa1dlist, varlist, factorlist=NULL, predic
     gap <- (salsa1dlist$gaps[(i-1)])
     term<- terms1D[[(i-1)]]
     interactionTerm<-NULL  #(salsa1dlist$interactionTerm[(i-1)])
-    baseModel <- eval(parse(text=paste("update(baseModel, .~. -", term, ")", sep="")))
+    
+    baseModel <- update(
+      baseModel,
+      as.formula(call("~", quote(.), call("-", quote(.), term)))
+    )
     
     if(removal==TRUE){
       #set.seed(seed.in)
       base_wo_coeff<-length(coef(baseModel))
       cv_without<-cv.gamMRSea(data, baseModel, K=salsa1dlist$fit.opts$K, cost=salsa1dlist$fit.opts$cost, s.eed = seed.in)$delta[2] 
+      if(salsa1dlist$modelType == "pointProcess"){
+        cv_without<- AIC(baseModel, k = log(salsa1dlist$fit.opts$N))
+      }
       fitStat_without<-get.measure(salsa1dlist$fitnessMeasure,'NA', baseModel, initDisp, salsa1dlist$fit.opts, printout)$fitStat  
     }
     
@@ -379,11 +424,17 @@ runSALSA1D<-function(initialModel, salsa1dlist, varlist, factorlist=NULL, predic
     splineParams[[varID[(i-1)]]]$knots= sort(output$aR)
     #}
     # update best model to have new knot locations and covariate back in model
-    tempModel<- eval(parse(text=paste("update(baseModel, .~. +", term, ")", sep="")))
+    tempModel <- update(
+      baseModel,
+      as.formula(call("~", quote(.), call("+", quote(.), term)))
+    )
     # calculate a cv score here too
     if(removal==TRUE){
       set.seed(seed.in)
       cv_with<- cv.gamMRSea(data=data, tempModel, K=salsa1dlist$fit.opts$K, cost=salsa1dlist$fit.opts$cost)$delta[2]  
+      if(salsa1dlist$modelType == "pointProcess"){
+        cv_with<- AIC(tempModel, k = log(salsa1dlist$fit.opts$N))
+      }
     }
     models<-output$models
     knotSites<-output$knotSites
@@ -393,8 +444,12 @@ runSALSA1D<-function(initialModel, salsa1dlist, varlist, factorlist=NULL, predic
         cat('Fitting Linear Model...')    
       }
     tempModel_lin<- eval(parse(text=paste("update(baseModel, . ~. +",varlist[(i-1)] , ")", sep="")))
+
     #set.seed(seed.in)
     cv_linear<- cv.gamMRSea(data=data, tempModel_lin, K=salsa1dlist$fit.opts$K, cost=salsa1dlist$fit.opts$cost, s.eed = seed.in)$delta[2]
+    if(salsa1dlist$modelType == "pointProcess"){
+      cv_linear<- AIC(tempModel_lin, k = log(salsa1dlist$fit.opts$N))
+    }
     
     cvid<-which(c(cv_initial, cv_with, cv_without, cv_linear)==min(na.omit(c(cv_initial, cv_with, cv_without, cv_linear))))
     
@@ -474,6 +529,8 @@ runSALSA1D<-function(initialModel, salsa1dlist, varlist, factorlist=NULL, predic
   }
   
   # turn the model data back into what came in
+  
+  #browser()
   
   #outTerms <- list(length(varlist_cyclicSplines))
   counter<-1
